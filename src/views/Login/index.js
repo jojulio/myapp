998ab185f3fe9878e7c45918590406313f91cb79
Julio Gonçalves <julio.goncalves@multiway.com.br>
2020-09-15 11:26:12 -0300

    auth login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { FaEnvelope, FaArrowRight, FaLock } from 'react-icons/fa';
 
-export default class Login extends React.Component {
-	render(){
-		return (
-			<div className="login-area">
-				<div className="container">
-					<div className="login-box ptb--100">
-						<form>
-							<div className="login-form-head">
-								<h4>Login</h4>
+import api from '../../services/api';
+import { login } from '../../services/auth';
+
+function initialState() {
+	return { email: 'admin@email.com', password: 'admin' };
+}
+
+const Login = () => {
+	const [values, setValues] = useState(initialState);
+	const [error, setError] = useState();
+	const history = useHistory();
+
+	function onChange(e) {
+		const { value, name } = e.target;
+		console.log(value, name)
+		setValues({
+			...values,
+			[name]: value
+		});
+	}
+
+	async function onSubmit(e) {
+		e.preventDefault();
+
+		const { email, password  } = values;
+
+		if (!email || !password) {
+			setError('Preencha e-mail e senha para continuar');
+		} else {
+			try {
+				const response = await api.post('/sessions', { email, password });
+				login(response.data.token);
+				history.push('/');
+			} catch (err) {
+				setError('Login e senha inválido');
+				setValues(initialState);
+			}
+		}
+	}
+
+	return (
+		<div className="login-area">
+			<div className="container">
+				<div className="login-box ptb--100">
+					<form onSubmit={onSubmit}>
+						<div className="login-form-head">
+							<h4>Login</h4>
+						</div>
+						<div className="login-form-body">
+							<div className="form-gp focused">
+								<label htmlFor="email">E-mail</label>
+								<input type="email" name="email" onChange={onChange} value={values.email} />
+								<FaEnvelope />
 							</div>
-							<div className="login-form-body">
-								<div className="form-gp">
-									<label htmlFor="email">E-mail</label>
-									<input type="email" id="email" />
-									<FaEnvelope />
-								</div>
-								<div className="form-gp">
-									<label htmlFor="password">Senha</label>
-									<input type="password" id="password" />
-									<FaLock />
-								</div>
-								<div className="submit-btn-area">
-									<button id="form_submit" type="submit">Entrar <FaArrowRight /> </button>
-								</div>
+							<div className="form-gp focused">
+								<label htmlFor="password">Senha</label>
+								<input type="password" name="password" onChange={onChange} value={values.password} />
+								<FaLock />
 							</div>
-						</form>
-					</div>
+							<div className="submit-btn-area">
+								<button id="form_submit" type="submit">Entrar <FaArrowRight /> </button>
+							</div>
+							{error && ( <div className="error-message">{error}</div>)}
+						</div>
+					</form>
 				</div>
 			</div>
-		);
-	}
- }
+		</div>
+	);
+}
+
+export default Login;
